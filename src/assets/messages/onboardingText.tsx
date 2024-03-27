9b3147d9e048d4d8824eb038cedcd013cdc602f0
Rohit Sahu <irohitsahu97@gmail.com>
2024-03-27 22:08:07 +0530

    fixed multiple use of welcome screen

--- src/assets/messages/onboardingText.tsx
+++ src/assets/messages/onboardingText.tsx
@@ -1,5 +1,16 @@
-export const WelcomeScreenText = {
-  welcome1: "Discover Our Products",
-  welcome2: "Explore Our Services",
-  welcome3: "Join Our Community",
+export const WelcomeScreenText: Record<string, string> = {
+  welcomeHeadingStep1: "Discover Our Products",
+  welcomeInfoStep1:
+    "Browse thousands of products, from fashion to tech. Find what you love, effortlessly.",
+  welcomeButtonStep1: "Next",
+
+  welcomeHeadingStep2: "Explore Our Services",
+  welcomeInfoStep2:
+    "Browse thousands of products, from fashion to tech. Find what you love, effortlessly.",
+  welcomeButtonStep2: "Next",
+
+  welcomeHeadingStep3: "Join Our Community",
+  welcomeInfoStep3:
+    "Browse thousands of products, from fashion to tech. Find what you love, effortlessly.",
+  welcomeButtonStep3: "Get Started",
 };
